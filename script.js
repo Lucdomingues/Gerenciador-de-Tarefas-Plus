81a3063c4d78c275581b6896d93762898cbfc6d4
Lucas Camargo <lojalucc@outlook.com>
2025-09-07 13:30:04 -0300

    feat: Concluir tarefas

--- script.js
+++ script.js
@@ -5,18 +5,38 @@ const lista_tarefas = document.getElementById("lista");
 // Função vai adicionar as tarefas na lista
 const adicionar_tarefa = (event) => {
     event.preventDefault(); // Previnindo o reload da página
-
+    
     // Cria li para dentro da lista com o valor do receptor
     const li = document.createElement("li");
     li.innerHTML = receptor.value;
+    li.className = "pendente";
+    
+    // Cria o input para concluir tarefas
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    li.appendChild(checkbox);
     lista_tarefas.appendChild(li);
 
     // Limpa o campo receptor
     receptor.value = "";
-
+    
 };
 
+// Função vai concluir as tarefas
+const conclui_tarefa =  (event) => {
+    if(event.checked){
+        event.parentElement.className = "concluida"; // Pega a li pai do check selecionado e adiciona o estilo de conclusão
+    } else {
+        event.parentElement.className = "pendente"; // Pega a li pai do check selecionado e adiciona o estilo de pendente
+    } 
+};
 
 // Escutando o evento de clique e chamando a função `adicionar_tarefa`
 btn_form.addEventListener("click", adicionar_tarefa);
- 
+lista_tarefas.addEventListener("change", (e) => {
+    // Precisa de uma validação aqui, pois o checkbox é criado dinamicamente
+    if(e.target.type === "checkbox"){        
+        conclui_tarefa(e.target);
+    }
+});
